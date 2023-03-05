19f362f83b696ebb4f35a5f24390a5dfbc69fa52
Valentina Sona <valentina.sona@mail.polimi.it>
2023-03-05 17:36:46 +0100

    moved original program to standalone

--- multi.cpp
+++ multi.cpp
@@ -2,6 +2,7 @@
 #include <queue>
 #include <cstring>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -25,19 +26,24 @@ enum InstrType {
 class CoreOUT {
 	private:
 		unsigned short PC;
-		bool active;
+		unsigned short CC_ID;
 	public:
 		CoreOUT(){
 			PC = 0;
-			active = false;
+			CC_ID = 0; //TODO: check this is coherent with usage
 		};
 
-		CoreOUT(unsigned short PC, bool active){
+		CoreOUT(unsigned short PC){
 			this->PC = PC;
-			this->active = active;
 		}
+
+		CoreOUT(unsigned short PC,unsigned short CC_ID){
+			this->PC = PC;
+			this->CC_ID = CC_ID;
+		}
+
 		unsigned short getPC(){return PC;}
-		bool isActive(){return active;}
+		unsigned short getCC_ID(){return CC_ID;}
 };
 
 // Wrapper around the 16bit instruction for easy retrieval of type, data and easy printing.
@@ -128,14 +134,14 @@ class Instruction {
 // Container for all the buffers - permits to instantiate a variable number of buffers.
 class Buffers {
 	private:
-		vector<queue<unsigned short>> buffers;
-		int size;
+		vector<queue<CoreOUT>> buffers;
+		int size; //2**W 
 	public:
 		Buffers(int n){ 
 			size = n;
 			buffers.reserve(n);
 			for (int i = 0; i < n; i++){
-				buffers.push_back(queue<unsigned short>());
+				buffers.push_back(queue<CoreOUT>());
 			}
 		}
 
@@ -151,6 +157,22 @@ class Buffers {
 			return buffers[target].empty();
 		}
 
+		// Expects to be told which is the buffer holding first character of sliding window.
+		bool hasInstructionReady(unsigned short head){
+			for (int i = 0; i < size -1 ; i++){ // Excludes the last buffer of the sliding window.
+
+				if (!buffers[(head + i) % size].empty()) return true;
+			}
+			return false;
+		}
+
+		unsigned short getFirstNotEmpty(unsigned short head){
+			for (int i = 0; i < size - 1 ; i++){ 	// Cannot return the inactive one.
+				if (!buffers[(head + i) % size].empty()) return (head+i) % size;
+			}
+			return size; //To be considered as all empty.
+		}
+
 		bool areAllEmpty(){
 			bool areEmpty = true;
 			for (int i = 0; i < size; i++){
@@ -162,23 +184,23 @@ class Buffers {
 			return areEmpty;
 		}
 
-		unsigned short getPC(unsigned short target){
-			unsigned short PC = buffers[target].front();
+		CoreOUT getPC(unsigned short target){
+			CoreOUT PC = buffers[target].front();
 			return PC;
 		}
 
-		unsigned short popPC(unsigned short target){
-			unsigned short PC = buffers[target].front();
+		CoreOUT popPC(unsigned short target){
+			CoreOUT PC = buffers[target].front();
 			buffers[target].pop();
 			return PC;
 		}
 
-		void pushTo(unsigned short target, unsigned short PC){
+		void pushTo(unsigned short target, CoreOUT PC){
 
 			if (target < size) 
 				buffers[target].push(PC);
 			else 
-				fprintf(stderr, "[X] Pushing to non-existing buffer.");
+				fprintf(stderr, "[X] Pushing to non-existing buffer.\n");
 		}
 };
 
@@ -196,7 +218,8 @@ class Core {
 		//Inter-phase registers
 		Instruction* s12;
 		Instruction* s23;
-		unsigned short PC12;
+		CoreOUT CO12;
+		CoreOUT CO23;
 
 		//settings
 		bool verbose;
@@ -209,7 +232,8 @@ class Core {
 			running = true;
 			s12 = NULL;
 			s23 = NULL;
-			PC12 = 0;
+			CO12 = CoreOUT();
+			CO23 = CoreOUT();
 			verbose = dbg;
 		}
 
@@ -219,7 +243,8 @@ class Core {
 			running = true;
 			s12 = NULL;
 			s23 = NULL;
-			PC12 = 0;
+			CO12 = CoreOUT();
+			CO23 = CoreOUT();
 
 		}
 
@@ -239,23 +264,26 @@ class Core {
 
 		Instruction* getStage12(){return s12;}
 		Instruction* getStage23(){return s23;}
-		unsigned short getPC12(){return PC12;}
+		CoreOUT getCO12(){return CO12;}
+		CoreOUT getCO23(){return CO23;}
 
 		void stage1(){
 			// Sets s12 to NULL if 
 			s12 = NULL;
 		}
-		void stage1(unsigned short PC){
-			s12 = &program[PC];
-			PC12 = PC;
-			if (verbose) { printf("\t(PC%d)(S1) ",PC); s12->printType(PC); printf("\n");}
+
+		// Multichar version
+		void stage1(CoreOUT bufferOUT){
+			s12 = &program[bufferOUT.getPC()];
+			CO12 = bufferOUT;
+			if (verbose) { printf("\t(PC%d)(S1) ",bufferOUT.getPC()); s12->printType(bufferOUT.getPC()); printf("\n");}
 		};
 
 		void stage2(){
 			s23 = NULL;
 		}
 
-		CoreOUT stage2(unsigned short sPC12, Instruction* stage12, char currentChar){
+		CoreOUT stage2(CoreOUT sCO12, Instruction* stage12, char currentChar){
 			// Stage 2: get next PC and handle ACCEPT
 			s23 = stage12;
 			CoreOUT newPC;
@@ -263,7 +291,7 @@ class Core {
 			accept = false;
 			valid = false;
 
-			if (verbose) { printf("\t(PC%d)(S2) ", sPC12); stage12->printType(sPC12);printf("\n");}
+			if (verbose) { printf("\t(PC%d)(S2) ", sCO12.getPC()); stage12->printType(sCO12.getPC());printf("\n");}
 
 			switch(stage12->getType()){
 
@@ -274,13 +302,13 @@ class Core {
 
 				case SPLIT:
 					valid = true;
-					newPC = CoreOUT( sPC12+1 , true);
+					newPC = CoreOUT( sCO12.getPC()+1 , sCO12.getCC_ID());
 					break;
 
 				case MATCH:
 					if (char(stage12->getData())==currentChar){
 						valid = true;
-						newPC = CoreOUT( sPC12+1, false);
+						newPC = CoreOUT(sCO12.getPC()+1, sCO12.getCC_ID()+1);
 						if (verbose) { printf("\t\tCharacters matched: input %c to %c\n", currentChar, char(stage12->getData()));}
 					} else {
 						if (verbose) { printf("\t\tCharacters not matched: input %c to %c\n", currentChar, char(stage12->getData()));}
@@ -290,7 +318,7 @@ class Core {
 
 				case JMP:
 					valid = true;
-					newPC = (CoreOUT(stage12->getData(), true));
+					newPC = (CoreOUT(stage12->getData(), sCO12.getCC_ID()));
 					break;
 
 				case END_WITHOUT_ACCEPTING:
@@ -301,7 +329,7 @@ class Core {
 				case MATCH_ANY:
 					valid = true;
 					if (verbose) { printf("\t\tCharacters matched: input %c to ANY\n", currentChar );}
-					newPC = (CoreOUT(sPC12+1, false));
+					newPC = (CoreOUT(sCO12.getPC()+1, (sCO12.getCC_ID()+1)));
 					break;
 
 				case ACCEPT_PARTIAL:
@@ -312,7 +340,7 @@ class Core {
 				case NOT_MATCH:
 					if (char(stage12->getData())!=currentChar){
 						valid = true;
-						newPC = CoreOUT( sPC12+1, true);
+						newPC = CoreOUT( sCO12.getPC()+1, sCO12.getCC_ID());
 					} else {
 						newPC = CoreOUT();
 					}
@@ -326,9 +354,10 @@ class Core {
 			return newPC;
 		};
 
-		CoreOUT stage3(Instruction* s23){
-			if (verbose) { printf("\t(S3)"); s23->printType(-1); printf("\n");}
-			CoreOUT newPC = CoreOUT(s23->getData(), true);
+		CoreOUT stage3(CoreOUT sCO23, Instruction* stage23){
+			//TODO: add a normal call without hardcoded -1
+			if (verbose) { printf("\t(S3)"); stage23->printType(-1); printf("\n");}
+			CoreOUT newPC = CoreOUT(stage23->getData(), sCO23.getCC_ID());
 
 			return newPC;
 		};
@@ -342,7 +371,17 @@ class Manager {
 
 		// Manager signal
 		unsigned short currentChar;
-		bool bufferSelection;
+		unsigned short CC_ID;
+
+		//TODO: this needs to become logic for interacting w/buffers. Should point to first
+		//buffer in sliding window (CC_ID = 0)
+		//Head?
+		unsigned short bufferHEAD;
+
+		//TODO: W?/CC_ID both.
+		// [CC_ID] [CC_ID + 1] ... [] CharID = currentChar + CC_ID
+		vector<bool> CCIDBitmap; 
+		unsigned short W;
 
 		//settings
 		bool verbose;
@@ -352,14 +391,28 @@ class Manager {
 			core = c;
 			buffers = b;
 			verbose = dbg;
+			//TODO: parametrize W.
+			W = 2;
+			CCIDBitmap.reserve(pow(2,W));
+			for (int i = 0; i < pow(2,W); i++){
+				CCIDBitmap.push_back(false);
+			}
+
+
 		}
 
-		// TODO: add RunMultiChar.
-		bool runBase(const char* input){
+		void updateActiveith(){
+			//TODO: push or pull? is it the manager checking? OR core/buffers notifying? Seems notifying from ith active diagram. 
+			//TODO: on push to buffer, pop from buffer, end of clock cycle?
+		}
+
+		bool runMultiChar(const char* input){
 			
 			// Signals handled by manager
-			currentChar = 0;
-			bufferSelection = 0;
+			currentChar = 0; // In MultiChar it refers to first character in sliding window.
+
+			// Should always point to the head of the queue.
+			bufferHEAD = 0; 
 
 			// Local variables to emulate clock cycles.
 			int CC = 0;
@@ -369,55 +422,56 @@ class Manager {
 			bool stage3Ready = false;
 			Instruction* s12 = NULL;
 			Instruction* s23 = NULL;
-			unsigned short PC12;
+			CoreOUT CO12 = CoreOUT();
+			CoreOUT CO23 = CoreOUT();
 
 			// Reset to known empty state.
 			core->reset();
 			buffers->flush();
 
 			// Load first instruction PC.
-			buffers->pushTo(bufferSelection, 0);
+			buffers->pushTo(bufferHEAD, 0);
+			CCIDBitmap[0] = true;
 
 			if (verbose) printf("\nInitiating match of string %s\n", input);
 
 			// Simulate clock cycle
 			while (!core->isAccepted() && core->isRunning()){
 			
+				//TODO: fix print w/ additional info
+				if (verbose) printf("[CC%d] Char:%c, Active buffer: FIFO%d\n", CC, input[currentChar+CC_ID], bufferHEAD);
 
-				if (verbose) printf("[CC%d] Char:%c, Active buffer: FIFO%d\n", CC, input[currentChar], bufferSelection);
-
-				/* To simulate the stages being concurrent, all values used by stages 2 and 3 
+				/* READ
+				 * To simulate the stages being concurrent, all values used by stages 2 and 3 
 				 * must be read at the start of the cycle, emulating values being read on rise. */
 
 				// Check at the start whether each stage meets the conditions for being executed.
-				stage1Ready = !buffers->isEmpty(bufferSelection);
-				stage2Ready = core->isStage2Ready();
-				stage3Ready = core->isStage3Ready();
+				//stage1Ready = !buffers->isEmpty(bufferHEAD); //TODO: new verification of stage1ready to account for multichar
+				stage1Ready = !buffers->hasInstructionReady(); //TODO: new verification of stage1ready to account for multichar
+				stage2Ready = core->isStage2Ready(); 
+				stage3Ready = core->isStage3Ready(); 
+
 				// Save the inter-stage registers for use.
 				s12 = core->getStage12();
 				s23 = core->getStage23();
-				PC12 = core->getPC12();
+				CO12 = core-> getCO12();
+				CO23 = core-> getCO23();
 
 				/* EXEC */
 
-				// Stage 1: retrieve newPC from active buffer and load instruction.
-				if (stage1Ready) core->stage1(buffers->getPC(bufferSelection));
-				else {core->stage1();}
+				// Stage 1: retrieve newPC from active buffer and load instruction. 
+				if (stage1Ready) core->stage1(buffers->getPC(buffers->getFirstNotEmpty(bufferHEAD)));
+				else {core->stage1();} //Empty run to clear signals.
 
 				// Stage 2: sets valid, running and accept signals. 
 				if (stage2Ready) {
-					newPC = core->stage2(PC12, s12, input[currentChar]);
+					newPC = core->stage2(CO12, s12, input[currentChar+CO12.getCC_ID()]);
 
 					// Handle the returned value, if it's a valid one. 
 					if (core->isValid()){
+						if (verbose) printf("\t\tPushing PC%d to FIFO%x\n", newPC.getPC(), bufferHEAD + newPC.getCC_ID()); 
 						// Push to correct buffer
-						if (newPC.isActive()){
-							if (verbose) printf("\t\tPushing PC%d to active FIFO%x\n", newPC.getPC(), bufferSelection);
-							buffers->pushTo(bufferSelection, newPC.getPC());
-						} else {
-							if (verbose) printf("\t\tPushing PC%d to inactive FIFO%x\n", newPC.getPC(), !bufferSelection);
-							buffers->pushTo(!bufferSelection, newPC.getPC());
-						}
+						buffers->pushTo(bufferHEAD + newPC.getCC_ID(), newPC.getPC()); 
 					// Invalid values that must be handled are returned by ACCEPT, ACCEPT_PARTIAL and END_WITHOUT_ACCEPTING.
 					// Apart from these, the only way for a computation to end is by reaching end of string without ACCEPT.
 					} else if (core->isAccepted()){
@@ -426,21 +480,17 @@ class Manager {
 						return false;
 					} 
 
-				} else { core->stage2(); }
+				} else { core->stage2(); } //Empty run to clear signals
 
 				// Stage 3: Only executed by a SPLIT instruction
 				if (stage3Ready){
 
 					newPC = core->stage3(s23);
-					
+
+					if (verbose) printf("\t\tPushing PC%d to FIFO%x\n", newPC.getPC(), bufferHEAD + newPC.getCC_ID()); 
 					// Push to correct buffer
-					if (newPC.isActive()){
-						if (verbose) printf("\t\tPushing PC%d to active FIFO%x\n", newPC.getPC(), bufferSelection);
-						buffers->pushTo(bufferSelection, newPC.getPC());
-					} else {
-						if (verbose) printf("\t\tPushing PC%d to inactive FIFO%x\n", newPC.getPC(), !bufferSelection);
-						buffers->pushTo(!bufferSelection, newPC.getPC());
-					}
+					buffers->pushTo(bufferHEAD + newPC.getCC_ID(), newPC.getPC()); 
+
 				}
 
 				/* WRITEBACK 
@@ -449,16 +499,20 @@ class Manager {
 				// Only if stage 1 was executed, consume the instruction that was loaded from the buffer.
 				// Otherwise, it would risk consuming a value added by stage 2 in the same cycle.
 				if (stage1Ready){
-					
-					if (verbose) printf("\t\tConsumed PC%d from active FIFO%x. Continue operating on char %c\n", buffers->getPC(bufferSelection), bufferSelection, input[currentChar]);
-					buffers->popPC(bufferSelection);				
+					if (verbose) printf("\t\tConsumed PC%d from FIFO%x, relating to character %c\n", buffers->getPC(buffers->getFirstNotEmpty(bufferHEAD)), buffers->getFirstNotEmpty(bufferHEAD), input[currentChar + core->getCO12().getCC_ID()]);
+					buffers->popPC(buffers->getFirstNotEmpty(bufferHEAD));				
 				} 
-				// If buffer was empty both now and at the start of the cycle, it's safe to deem it empty and switch to handling a new character
-				// TODO: Test if this fails in the face of multiple SPLITs
-				else if (buffers->isEmpty(bufferSelection)){ // Buffer ping pong.
-					currentChar ++;
-					if (verbose) printf("\t\tFIFO%x is empty, activating FIFO%x and moving to char %c\n", bufferSelection, !bufferSelection, input[currentChar]);
-					bufferSelection = !bufferSelection;
+				//TODO: this becomes sliding window update (update also head/buffer select when sliding. Needs to account for stage 3 too, which it didn't in base case!!
+				// If buffer was empty both now and at the start of the cycle, it's safe to deem it empty and switch to handling a new character TODO: check logic with multichar
+				// TODO: cannot be else if anymore, can it?
+				// TODO: updateBitmap() here
+				if (buffers->isEmpty(bufferHEAD)){ // Conditions for sliding the window. checkBitmap()
+					//TODO: unsigned short i = slidingOP()
+					currentChar ++; // Move the window + i
+					bufferHEAD ++; // TODO +i
+					if (verbose) printf("\t\tFIFO%x is empty, activating FIFO%x and moving to char %c\n", bufferHEAD, !bufferHEAD, input[currentChar]);
+					bufferHEAD = !bufferHEAD; 
+					//TODO: I have my theoretical how-to-slide approach. Only need to keep buffers and CC_IDs coordinated. 
 				}
 				
 				CC++;
@@ -470,6 +524,7 @@ class Manager {
 			return false;
 		}
 };
+
 
 // Wrapper class that holds and inits all components.
 class SoftwareCICERO {
@@ -487,11 +542,22 @@ class SoftwareCICERO {
 		bool hasProgram = false;
 
 	public:
-		SoftwareCICERO(bool dbg = false){
+		SoftwareCICERO(unsigned short W = 1, bool dbg = false){
 
-			multiChar = false;
+			if (W == 0) W = 1;
+
+			if (W > 1) {
+
+				multichar = true;
+				buffers = Buffers(pow(2,W));
+				
+			} else { 
+
+				multiChar = false;
+				buffers = Buffers(2);
+			}
+
 			hasProgram = false;
-			buffers = Buffers(2);
 			core = Core(&program[0], dbg);
 			manager = Manager(&buffers, &core, dbg);
 			verbose = dbg;
@@ -512,18 +578,18 @@ class SoftwareCICERO {
 					program[i] = Instruction(instr);
 					fscanf(fp,"\n");
 
-					//Prettyprint instruction
+					// Pretty print instructions
 					if (verbose) program[i].print(i);
 				}
 
 				if ( i == INSTR_MEM_SIZE && !feof(fp)) {
-					fprintf(stderr, "[X] Program memory exceeded. Only the first %x instructions were read.", INSTR_MEM_SIZE);
+					fprintf(stderr, "[X] Program memory exceeded. Only the first %x instructions were read.\n", INSTR_MEM_SIZE);
 				}
 
 				hasProgram = true;
 
 			} else {
-				fprintf(stderr, "[X] Could not open program file for reading.");
+				fprintf(stderr, "[X] Could not open program file for reading.\n");
 			}
 		};
 		
@@ -532,18 +598,14 @@ class SoftwareCICERO {
 			//TODO: checks on input string not being malformed.
 
 			if (!hasProgram){
-				fprintf(stderr, "[X] No program is loaded to match the string against.");
+				fprintf(stderr, "[X] No program is loaded to match the string against.\n");
 				return NULL;
 			} 
 
-			if (!multiChar){
-				return manager.runBase(input);
-			} else return false;
+			return manager.runMultiChar(input);
 		}
 };
 
-//TODO: testing
-//TODO: init functions to make sure core, buffer and manager reset correctly.
 //TODO: split into headers and files to make it work like a decent library.
 
 int main(void){
@@ -555,8 +617,6 @@ int main(void){
 	if(CICERO.match("RKMS")) printf("regex %d 	, input %d (len: %d)	, match True\n",0,0,256);
 	else printf("regex %d 	, input %d (len: %d)	, match False\n",0,0,256);
 
-//	if(CICERO.match("b'MSIIGATRLQNDKSDTYSAGPCYAGGCSAFTPRGTCGKDWDLGEQTCASGFCTSQPLCARIKKTQVCGLRYSSKGKDPLVSAEWDSRGAPYVRCTYDADLIDTQAQVDQFVSMFGESPSLAERYCMRGVKNTAGELVSRVSSDADPAGGWCRKWYSAHRGPDQDAALGSFCIKNPGAADCKCINRASDPVYQKVKTLHAYPDQCWYVPCAADVGELKMGTQRDTPTNCPTQVCQIVFNMLDDGSVTMDDVKNTINCDFSKYVPPPPPPKPTPPTPPTPPTPPTPPTPPTPPTPRPVHNRKVMFFVAGAVLVAILISTVRW'")) printf("regex %d 	, input %d (len: %d)	, match True\n",0,1,320);
-//	else printf("regex %d 	, input %d (len: %d)	, match False\n",0,1,320);
 
 	return 0;
 }
